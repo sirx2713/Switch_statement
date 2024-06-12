02d02dd42e1956a859bf10c52fc0de059950a666
Mabende <belindamabende@gmail.com>
2024-06-12 18:43:40 +0200

    Initial Commit

---
+++ Switch_statement.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let yearsCoding = 5
+
+switch yearsCoding{
+case 0:
+    print("Can not be")
+case 1:
+    print("That is too short")
+case 2:
+    print("Not exact")
+case 3:
+    print("Can be correct")
+case 4:
+    print("Might be")
+default:
+    print("Are you experienced")
+}
